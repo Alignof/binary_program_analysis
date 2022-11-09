3934da1a9943bb39db18bdc6890c580d757ce664
Alingof <Alignof@outlook.com>
2022-11-09 16:27:38 +0900

    [add] add create_func_table to ElfLoader

--- src/loader/elf.rs
+++ src/loader/elf.rs
@@ -3,7 +3,7 @@ mod elf_64;
 
 use std::collections::HashMap;
 use memmap::Mmap;
-use crate::loader::Loader;
+use crate::loader::{get_u32, get_u64, Loader, Function};
 use elf_64::elf_header::ElfHeader64;
 use elf_64::program_header::ProgramHeader64;
 use elf_64::section_header::SectionHeader64;
@@ -52,20 +52,65 @@ pub struct ElfLoader {
     pub elf_header: Box<dyn ElfHeader>,
     pub prog_headers: Vec<ProgramHeader64>,
     pub sect_headers: Vec<SectionHeader64>,
+    pub functions: Vec<Function>,
     pub mem_data: Mmap,
 }
 
 impl ElfLoader {
+    fn create_func_table(mmap: &[u8], sect_headers: &Vec<SectionHeader64>) -> Vec<Function> {
+        let symtab = sect_headers.iter()
+            .find_map(|s| {
+                if s.sh_name == ".symtab" {
+                    return Some(s);
+                }
+                None
+            });
+        let strtab = sect_headers.iter()
+            .find_map(|s| {
+                if s.sh_name == ".strtab" {
+                    return Some(s);
+                }
+                None
+            });
+
+        const ST_SIZE: usize = 32;
+        let mut functions: Vec<Function> = Vec::new();
+        if let (Some(symtab), Some(strtab)) = (symtab, strtab) {
+            for symtab_off in (symtab.sh_offset .. symtab.sh_offset + symtab.sh_size).step_by(ST_SIZE) {
+                let st_name_off = get_u32(mmap, symtab_off as usize);
+                let st_name = mmap[(strtab.sh_offset + st_name_off as u64) as usize ..]
+                    .iter()
+                    .take_while(|c| **c as char != '\0')
+                    .map(|c| *c as char)
+                    .collect::<String>();
+                let st_addr = get_u64(mmap, (symtab_off + 16) as usize);
+                let st_size = get_u64(mmap, (symtab_off + 24) as usize);
+
+                functions.push(
+                    Function {
+                        name: st_name,
+                        addr: st_addr,
+                        size: st_size,
+                    }
+                );
+            }
+        }
+
+        functions
+    }
+
     pub fn new(mapped_data: Mmap) -> Box<dyn Loader> {
         let new_elf = ElfHeader64::new(&mapped_data);
         let new_prog = ProgramHeader64::new(&mapped_data, &new_elf);
         let new_sect = SectionHeader64::new(&mapped_data, &new_elf);
+        let new_func = Self::create_func_table(&mapped_data, &new_sect);
 
         Box::new(
             ElfLoader {
                 elf_header: new_elf,
                 prog_headers: new_prog,
                 sect_headers: new_sect,
+                functions: new_func,
                 mem_data: mapped_data,
             }
         )
@@ -86,7 +131,7 @@ trait SectionHeader {
     fn type_to_str(&self) -> &'static str;
     fn show(&self, id: usize);
     fn dump(&self, mmap: &[u8]);
-    fn inst_analysis(&self, inst_list: &HashMap<String, u32>, mmap: &[u8]);
+    fn inst_analysis(&self, inst_list: &mut HashMap<String, u32>, mmap: &[u8]);
 }
 
 impl Loader for ElfLoader {
@@ -134,9 +179,10 @@ impl Loader for ElfLoader {
     }
 
     fn analysis(&self) {
-        let inst_list = HashMap::new();
+        let mut inst_list = HashMap::new();
         for sect in self.sect_headers.iter() {
-            sect.inst_analysis(&inst_list, &self.mem_data);
+            sect.inst_analysis(&mut inst_list, &self.mem_data);
         }
+        println!("{:#?}", inst_list);
     }
 }
